50d0ed74b0ea2029912117c48764a406dc3c2502
aludpasopati-byte <aludpasopati@gmail.com>
2026-05-12 18:02:01 +0700

    ibu string

--- ibu.h
+++ ibu.h
@@ -15,4 +15,11 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
-   
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
